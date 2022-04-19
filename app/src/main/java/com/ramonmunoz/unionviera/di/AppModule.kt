4e6002121aae281b7e72d6a1ec965d7fe9caf30f
Mario Monzón <hola@mariomonzon.dev>
2022-04-19 06:03:29 +0100

    Setup Dagger hilt

---
+++ app/src/main/java/com/ramonmunoz/unionviera/di/AppModule.kt
@@ -0,0 +1,46 @@
+package com.ramonmunoz.unionviera.di
+
+import android.app.Application
+import androidx.room.Room
+import com.ramonmunoz.unionviera.feature_notes.data.data_source.NoteDatabase
+import com.ramonmunoz.unionviera.feature_notes.data.repository.NoteRepositoryImpl
+import com.ramonmunoz.unionviera.feature_notes.domain.repository.NoteRepository
+import com.ramonmunoz.unionviera.feature_notes.domain.use_case.DeleteNote
+import com.ramonmunoz.unionviera.feature_notes.domain.use_case.GetNotes
+import com.ramonmunoz.unionviera.feature_notes.domain.use_case.NoteUseCases
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideNoteDatabase(app: Application): NoteDatabase{
+        return  Room.databaseBuilder(
+            app,
+            NoteDatabase::class.java,
+            NoteDatabase.DATABASE_NAME
+        ).build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideNoteRepository(db: NoteDatabase): NoteRepository {
+        return NoteRepositoryImpl(db.noteDao)
+    }
+
+
+    @Provides
+    @Singleton
+    fun provideNoteUseCases(repository: NoteRepository): NoteUseCases{
+        return NoteUseCases(
+            getNotes = GetNotes(repository),
+            deleteNote = DeleteNote(repository)
+        )
+    }
+}
